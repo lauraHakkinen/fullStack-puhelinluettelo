4f23d7c209af5fd7a6f18a5f591932a6030c8d00
Laura Häkkinen <laura.a.hakkinen@gmail.com>
2022-07-07 22:25:51 +0300

    numerotietojen poisto tietokannasta ja virheidenkäsittely: versiot 3.15 ja 3.16

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
 const express = require('express')
+const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+require('dotenv').config()
 const Person = require('./models/person')
 
-const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
@@ -12,29 +12,6 @@ app.use(express.static('build'))
 morgan.token('content', (req => {
   return JSON.stringify(req.body)
 }))
-
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "305-23579824",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  }
-]
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
@@ -45,27 +22,30 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/info', (req, res) => {
-  res.send(`<p>Phonebook has info for ${persons.length} people</p> 
-            <p>${new Date()}</p>`)
+  Person.countDocuments({}).then(len => {
+    res.send(`<p>Phonebook has info for ${len} people</p> 
+    <p>${new Date()}</p>`)
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id);
-
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
-  
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
@@ -92,6 +72,18 @@ const unknownEndpoint = (req, res) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
